7ed743de9baa89ec41b7d3d8797ea7a0ea158dec
Mohamed Ben-yghil <med.2016.ben@gmail.com>
2022-07-20 14:10:46 +0100

    new coments

--- src/main/java/com/wafa/kpi/KpiApplication.java
+++ src/main/java/com/wafa/kpi/KpiApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KpiApplication {
 	//comment
+	//comment 2
+	//comment 3
 	public static void main(String[] args) {
 		SpringApplication.run(KpiApplication.class, args);
 	}
