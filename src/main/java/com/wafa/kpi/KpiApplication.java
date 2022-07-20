1b2b7e04ee8158636adfff748b42f6d9d53ab038
Mohamed Ben-yghil <med.2016.ben@gmail.com>
2022-07-20 14:05:48 +0100

    commited master

--- src/main/java/com/wafa/kpi/KpiApplication.java
+++ src/main/java/com/wafa/kpi/KpiApplication.java
@@ -2,9 +2,11 @@ package com.wafa.kpi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//annotation spring app
 @SpringBootApplication
 public class KpiApplication {
+
+	//comment
 	public static void main(String[] args) {
 		SpringApplication.run(KpiApplication.class, args);
 	}
